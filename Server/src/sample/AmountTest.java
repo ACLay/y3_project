a8a468f2259ea03aaef730f3a9b8e0be3f3dc378
Alexander Lay <acl1g12@soton.ac.uk>
2014-11-13 13:34:49 +0000

    Moved jScience tests to apropriate class

---
+++ Server/src/sample/AmountTest.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javax.measure.quantity.Energy;
+import javax.measure.unit.SI;
+
+import org.jscience.physics.amount.Amount;
+
+import Model.Car;
+
+public class AmountTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){
+		//create a car
+		Car tesla = new Car("Tesla Model S", Amount.valueOf(500, SI.KILOMETER) ,Amount.valueOf("306 MJ").to(SI.MEGA(SI.JOULE)));
+		
+		//output energy needed to go 1km
+		Amount<Energy> e = tesla.chargeNeededToTravel(Amount.valueOf(1, SI.KILOMETER));
+		
+		System.out.println("Energy per kilometer:\n" + e.to(SI.KILO(SI.JOULE)));
+		//bounds on what e might be, (based on computer calculation inaccuracy?)
+		System.out.println("Min: " + e.getMinimumValue());
+		System.out.println("Max: " + e.getMaximumValue());
+		//estimated total charge based on charge/km
+		System.out.println(e.getEstimatedValue() * 500);
+		
+		//experimenting with units
+		@SuppressWarnings("rawtypes")
+		Amount speed = Amount.valueOf(25, SI.METER).divide(Amount.valueOf(5, SI.SECOND));
+		
+		System.out.println(speed);
+		System.out.println(speed.getUnit());
+		
+		System.out.println(speed.times(Amount.valueOf(2, SI.SECOND)));
+		
+		System.out.println(Amount.valueOf(20, SI.METER).divide(speed));
+		
+		System.out.println(Amount.valueOf("306 MJ").to(SI.JOULE));
+	
+	}
+
+}
