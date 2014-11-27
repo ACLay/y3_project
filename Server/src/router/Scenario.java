a735ae27cbd00a63571cde01f71642a4061b99c7
Alexander Lay <acl1g12@soton.ac.uk>
2014-11-27 22:11:18 +0000

    Changed visibility of class variables so a subclass can manipulate them

--- Server/src/router/Scenario.java
+++ Server/src/router/Scenario.java
@@ -6,20 +6,20 @@ import Model.Charger;
 
 public class Scenario {
 	
-	private Graph g;
-	private Charger start;
-	private Charger finish;
-	private Car car;
+	protected Graph graph;
+	protected Charger start;
+	protected Charger finish;
+	protected Car car;
 	
 	public Scenario(Graph g, Charger start, Charger finish, Car car){
-		this.g = g;
+		this.graph = g;
 		this.start = start;
 		this.finish = finish;
 		this.car = car;
 	}
 	
 	public Graph getGraph(){
-		return g;
+		return graph;
 	}
 	
 	public Charger getStart(){
