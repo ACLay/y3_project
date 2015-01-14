4fc087a9e5089913257c657139788a1c4ae82cac
Alexander Lay <acl1g12@soton.ac.uk>
2015-01-14 18:22:46 +0000

    Added routers that use a single measure of the fastest, or chargiest state to prune the search space

---
+++ Server/src/router/ChargePrunedQueueRouter.java
@@ -0,0 +1,57 @@
+package router;
+
+import java.util.HashMap;
+
+import Model.Charger;
+
+public class ChargePrunedQueueRouter extends QueueRouter {
+	
+	HashMap<Charger, State> chargestStates = new HashMap<Charger, State>();
+	
+	protected void addState(State s){
+		
+		/*
+		 * store fastest state for each node
+		 * store chargiest state for each node
+		 * 
+		 * new candidate state
+		 * case: faster than fastest state
+		 * 		> add candidate, set as fastest
+		 * case: chargier than chargiest state
+		 * 		> add candidate, set as chargiest
+		 * case: chargier than fastest, faster than chargiest
+		 * 		> add candidate (fastest may not have enough charge, this may be faster than chargiest)
+		 */
+		
+		Charger location = s.getLocation();
+		
+		boolean addable = false;
+		
+		if(chargestStates.containsKey(location)){
+			State chargest = chargestStates.get(location);
+			//A state for a location should be checked:
+			//if it's the fastest
+			if(s.getTime().isLessThan(chargest.getTime())){
+				addable = true;
+			}
+			//if its got the most charge
+			if(s.getEnergy().isGreaterThan(chargest.getEnergy())){
+				chargestStates.put(location, s);
+				addable = true;
+			}
+			
+		} else {
+			chargestStates.put(location, s);
+			addable = true;
+		}
+		
+		created++;
+		
+		if(addable){
+			pq.add(s);
+			stored++;
+		}
+		
+	}
+	
+}
---
+++ Server/src/router/TimePrunedQueueRouter.java
@@ -0,0 +1,57 @@
+package router;
+
+import java.util.HashMap;
+
+import Model.Charger;
+
+public class TimePrunedQueueRouter extends QueueRouter {
+	
+	HashMap<Charger, State> fastestStates = new HashMap<Charger, State>();
+	
+	protected void addState(State s){
+		
+		/*
+		 * store fastest state for each node
+		 * store chargiest state for each node
+		 * 
+		 * new candidate state
+		 * case: faster than fastest state
+		 * 		> add candidate, set as fastest
+		 * case: chargier than chargiest state
+		 * 		> add candidate, set as chargiest
+		 * case: chargier than fastest, faster than chargiest
+		 * 		> add candidate (fastest may not have enough charge, this may be faster than chargiest)
+		 */
+		
+		Charger location = s.getLocation();
+		
+		boolean addable = false;
+		
+		if(fastestStates.containsKey(location)){
+			State fastest = fastestStates.get(location);
+			//A state for a location should be checked:
+			//if it's the fastest
+			if(s.getTime().isLessThan(fastest.getTime())){
+				fastestStates.put(location, s);
+				addable = true;
+			}
+			//if its got the most charge
+			if(s.getEnergy().isGreaterThan(fastest.getEnergy())){
+				addable = true;
+			}
+			
+		} else {
+			fastestStates.put(location, s);
+			addable = true;
+		}
+		
+		created++;
+		
+		if(addable){
+			pq.add(s);
+			stored++;
+		}
+		
+	}
+	
+}
