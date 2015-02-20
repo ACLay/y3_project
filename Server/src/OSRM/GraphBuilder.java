99598bc3211d6d2e7f0109ea223fbef7cb1caf1e
Alexander Lay <acl1g12@soton.ac.uk>
2015-02-20 17:45:56 +0000

    Class to build graphs using chargers and OSRM calls

---
+++ Server/src/OSRM/GraphBuilder.java
@@ -0,0 +1,104 @@
+package OSRM;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.measure.unit.SI;
+
+import org.jscience.physics.amount.Amount;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import router.Edge;
+import router.graph.Graph;
+import router.graph.RamGraph;
+import Model.Charger;
+
+public class GraphBuilder {
+
+	QueryBuilder qb;
+
+	public GraphBuilder(QueryBuilder queryBuilder){
+		qb = queryBuilder;
+	}
+
+	public Graph buildGraph(Collection<Charger> chargers){
+		Graph graph = new RamGraph();
+		graph.addNodes(chargers);
+
+		Iterator<Charger> iter1 = chargers.iterator();
+		Iterator<Charger> iter2 = chargers.iterator();
+
+		while(iter1.hasNext()){
+			Charger startPoint = iter1.next();
+			while(iter2.hasNext()){
+				Charger endPoint = iter2.next();
+				if(!startPoint.equals(endPoint)){
+					Edge edge;
+					try {
+						edge = makeEdge(startPoint, endPoint);
+					} catch (Exception e) {
+						e.printStackTrace();
+						continue;
+					}
+					graph.addEdge(edge);
+				}
+			}
+		}
+
+		return graph;
+	}
+
+	public Edge makeEdge(Charger startPoint, Charger endPoint) throws UnroutableException, MalformedURLException, IOException, JSONException{
+		URL url;
+		try {
+			url = qb.routeBetween(startPoint, endPoint);
+		} catch (MalformedURLException e) {
+			return null;
+		}
+		try{
+			
+			JSONObject json = readURL(url);
+			int status = json.getInt("status");
+			if(status == 0){
+				JSONObject summary = json.getJSONObject("route_summary");
+				int meters = summary.getInt("total_distance");
+				int seconds = summary.getInt("total_time");
+				return new Edge(startPoint, endPoint, Amount.valueOf(meters, SI.METER) , Amount.valueOf(seconds, SI.SECOND));
+			} else {
+				throw new UnroutableException();
+			}
+		} catch (IOException e){
+			throw e;
+		} catch (JSONException je){
+			throw je;
+		}
+	}
+	
+	private static JSONObject readURL(URL url) throws IOException{
+		URLConnection yc = url.openConnection();
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(
+						yc.getInputStream()));
+		
+		StringBuilder urlData = new StringBuilder();
+		
+		String inputLine;
+		
+		while ((inputLine = br.readLine()) != null){
+			System.out.println(inputLine);
+			urlData.append(inputLine).append("\n");
+		}
+		br.close();
+
+		String jsonText = urlData.toString();
+		return new JSONObject(jsonText);
+	}
+
+}
