97f608eb5435cd27811ae31080645dcdf625db78
alex <alex@alex-desktop-mint>
2015-03-08 17:09:31 +0000

    Added "IEC60309 3P+N+E,6h,32A" type, which is found in new registry download.

--- Server/src/Model/connectors/Type.java
+++ Server/src/Model/connectors/Type.java
@@ -4,6 +4,7 @@ public enum Type {
 
 	CCS_COMBO ("CCS Combo"),
 	BS_1363 ("Domestic plug/socket type G (BS 1363)"),
+	IEC_60309 ("IEC60309 3P+N+E,6h,32A"),
 	IEC_TYPE_1 ("IEC 62196-2 type 1 (SAE J1772)"),
 	IEC_TYPE_2 ("IEC 62196-2 type 2"),
 	IEC_TYPE_3 ("IEC 62196-2 type 3"),
