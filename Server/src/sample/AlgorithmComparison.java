ced596fbb317e81bc7bd3442b975006ebb19fcde
alex <alex@alex-desktop-mint>
2015-03-27 00:06:02 +0000

    Initial commit for AlgorithmComparison. To compare EV routers against Dijkstra's for petrol vehicles.

---
+++ Server/src/sample/AlgorithmComparison.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class AlgorithmComparison {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//for each size of network
+			//build the network
+			//route it with a normal and electric vehicle
+			//output the data
+		
+	}
+
+}
