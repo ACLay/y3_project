b9f885f6065aa661985a695cd6a0a419ccd2ba9d
ACLay <electrical34@gmail.com>
2015-04-02 16:37:05 +0100

    Created comparators that work with just one component of the entire heuristic.

---
+++ Server/src/router/comparator/ChargeTimeStateComparator.java
@@ -0,0 +1,25 @@
+package router.comparator;
+
+import javax.measure.quantity.Duration;
+import javax.measure.quantity.Length;
+import javax.measure.quantity.Power;
+import javax.measure.quantity.Velocity;
+
+import org.jscience.physics.amount.Amount;
+
+import router.Geography;
+import router.State;
+import Model.Charger;
+
+public class ChargeTimeStateComparator extends AStarStateTimeComparator {
+
+	public ChargeTimeStateComparator(Charger endpoint, Amount<Velocity> maxSpeed, Amount<Power> fastestCharge) {
+		super(endpoint, maxSpeed, fastestCharge);
+	}
+	
+	protected Amount<Duration> heuristicTime(State s){
+		Amount<Length> distance = Geography.haversineDistance(s.getLocation(), endpoint);
+		return chargeTime(s,distance);
+	}
+
+}
