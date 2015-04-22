1779b8b037620d9abd9899f4416195b419f107d1
ACLay <electrical34@gmail.com>
2015-04-22 13:42:44 +0100

    Counts the number of edges and nodes in a folder graph

---
+++ Server/src/sample/FolderAnalyzer.java
@@ -0,0 +1,31 @@
+package sample;
+
+import java.util.Collection;
+
+import router.Edge;
+import router.graph.FolderGraph;
+import Model.Charger;
+
+public class FolderAnalyzer {
+
+	public static void main(String[] args) {
+		//FolderGraph full = new FolderGraph("./xml/edges/", "./xml/edges/edited_registry.xml");
+		FolderGraph reduced = new FolderGraph("./xml/powerCut2/", "./xml/powerCut2/nodes.xml");
+		
+		//analyze(full);
+		analyze(reduced);
+
+	}
+
+	public static void analyze(FolderGraph g){
+		Collection<Charger> chargers = g.getNodes();
+		System.out.println(chargers.size() + " chargers");
+		int edgeCount = 0;
+		for(Charger c : chargers){
+			Collection<Edge> edges = g.getEdgesFrom(c);
+			edgeCount += edges.size();
+		}
+		System.out.println(edgeCount + " edges");
+	}
+	
+}
